965ed0ed788de19c4c12e8ed816816370e2ddf2a
Vasile Lazar <vlazar184@gmail.com>
2026-09-16 20:51:06 +0300

    Add business logic for auth and security

---
+++ backend/Ledgr.BusinessLogic/BusinessLogic.cs
@@ -0,0 +1,22 @@
+﻿using Ledgr.BusinessLogic.Core;
+using Ledgr.BusinessLogic.Interfaces;
+using Ledgr.DataAccess.Context;
+
+namespace Ledgr.BusinessLogic;
+
+public class BusinessLogic
+{
+    private readonly LedgrDbContext _context;
+    private readonly HttpClient _httpClient;
+
+    public BusinessLogic(LedgrDbContext context, HttpClient httpClient)
+    {
+        _context = context;
+        _httpClient = httpClient;
+    }
+
+    public IAuthLogic GetAuthLogic()
+    {
+        return new AuthLogic(_context);
+    }
+}
---
+++ backend/Ledgr.BusinessLogic/Interfaces/IAuthLogic.cs
@@ -0,0 +1,10 @@
+﻿using Ledgr.Domain.Models.Auth;
+using Ledgr.Domain.Models.Service;
+
+namespace Ledgr.BusinessLogic.Interfaces;
+
+public interface IAuthLogic
+{
+    ServiceResponse Register(RegisterDto dto);
+    ServiceResponse Login(LoginDto dto);
+}
